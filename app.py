f04c48e89892812277ca7d611b7c13a35dcf68cb
adarsh-ask27 <pandeyaadarsh9798@gmail.com>
2026-02-02 21:34:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,15 @@ class VoiceRequest(BaseModel):
         allow_population_by_field_name = True
 
 
-@app.get("/")
-def root():
-    return {"status": "API running"}
+@app.get("/detect-voice")
+def honeypot_check(x_api_key: str = Header(None)):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+
+    return {
+        "status": "alive",
+        "message": "Honeypot check passed"
+    }
 
 @app.post("/detect-voice")
 def detect_voice(
