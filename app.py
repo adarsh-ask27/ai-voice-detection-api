fcf726a6a25a0ac2735ef212ee5321f37cd2d76c
adarsh-ask27 <pandeyaadarsh9798@gmail.com>
2026-02-02 20:38:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def detect_voice(data: VoiceRequest):
     try:
         audio_bytes = base64.b64decode(data.audio_base64)
 
-        if len(audio_bytes) < 15000:
-            raise HTTPException(status_code=400, detail="Audio too short")
+       """ if len(audio_bytes) < 15000:
+            raise HTTPException(status_code=400, detail="Audio too short")"""
 
         suffix = "." + data.audio_format.lower()
         with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as f:
